ac6e4a9fe6b670eac44f58983bfb2baf0e7c3bb4
Dr-Vell <davidzamora.2001@hotmail.es>
2023-06-11 16:54:09 +0200

    Obstacles Added between rooms
    Unless the player completes battles, obstacles won't disappear, preventing the player from just ignoring fights.

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,3 +3,10 @@ extends Node2D
 
 func _ready():
 	$Player.position=GameState.player_current_position
+	
+	if GameState.merchant_defeated:
+		$"Plants 1".queue_free()
+	if GameState.warrior_defeated:
+		$"Plants 2".queue_free()
+	if GameState.mino_defeated:
+		$"Plants 3".queue_free()
