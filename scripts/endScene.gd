880c38ee7f8478ea7fee5686de825e23fcdb1ec7
Dr-Vell <davidzamora.2001@hotmail.es>
2023-06-13 13:03:17 +0200

    Tutorial Scene Added
    This scene explains controls to the player

--- scripts/endScene.gd
+++ scripts/endScene.gd
@@ -3,13 +3,16 @@ extends Control
 
 func _ready():
 	$Timer.start()
-	GameState.player_current_motivation = 60
-	GameState.player_current_position= Vector2(0,0)
-	GameState.player_hit = 20
-	GameState.merchant_defeated = false
-	GameState.warrior_defeated = false
-	GameState.mino_defeated = false
 
 
 func _on_Timer_timeout():
-	get_tree().change_scene("res://scenes/menu.tscn")
+	if self.name=="Tutorial":
+		GameState.player_current_motivation = 60
+		GameState.player_current_position= Vector2(0,0)
+		GameState.player_hit = 20
+		GameState.merchant_defeated = false
+		GameState.warrior_defeated = false
+		GameState.mino_defeated = false
+		get_tree().change_scene("res://scenes/main.tscn")
+	else:
+		get_tree().change_scene("res://scenes/menu.tscn")
